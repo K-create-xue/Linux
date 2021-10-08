3f906d765954c0cf1f4c9e1107a1adf6e2ee83d4
K-create-xue <76169472+K-create-xue@users.noreply.github.com>
2021-10-08 22:39:10 +0800

    命名管道——读端

---
+++ pipe_read.c
@@ -0,0 +1,10 @@
+//用命名管道，实现读端
+//umask(0);
+//int ret=mkfifo("pipe_buf",0664>);
+//int fd=open("pipe_buf",O_RDONLY);
+//while(1){
+//read(fd,buf,1023);
+//}
+//close(fd);
+//                                                                                                              
+~  
